9c8b06155ec93d339fc7b17df2159d79488755c4
vojin-nisevic <nisevic.vojin@gmail.com>
2022-10-04 18:31:19 +0200

    creating elite war team service

--- src/app/services/el-war-team.service.ts
+++ src/app/services/el-war-team.service.ts
@@ -1,16 +1,46 @@
 import { Injectable, OnInit } from '@angular/core';
 import { AppService } from "../global/app.service";
+import { ElWarTeam } from "../models/el-war-team";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ElWarTeamService implements OnInit{
+export class ElWarTeamService implements OnInit {
 
-  private baseUrl: String = '';
+  private baseUrl: string = '';
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService, private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
-    this.baseUrl = this.appService.getBaseUrl();
+    this.baseUrl = this.appService.getBaseUrl() + '/ewteam';
   }
+
+  //fetch all el war teams
+  getAll() {
+    return this.httpClient.get<ElWarTeam[]>(this.baseUrl);
+  }
+
+  //fetch team by id
+  getTeamById(id: number) {
+    return this.httpClient.get<ElWarTeam>(this.baseUrl + '/find',
+      { params: { id: id } }
+    );
+  }
+
+  //add new team
+  addTeam(team: ElWarTeam) {
+    return this.httpClient.post<ElWarTeam>(this.baseUrl + '/add',
+      {ewteam: team}
+      );
+  }
+
+  //edit team
+  editTeam(team: ElWarTeam) {
+    return this.httpClient.put<ElWarTeam>(this.baseUrl + '/add',
+      {ewteam: team}
+      );
+  }
+
 }
